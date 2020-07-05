6806895e7aa5fa4e8a3320b0235a0ef54b4e5ea3
Alex Golubev <golubevaleksej7@gmail.com>
2020-07-05 12:22:56 +0200

    add test

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -19,7 +19,7 @@ public class Main extends Application {
         primaryStage.setMaxHeight(447);
         primaryStage.setMaxWidth(773);
         primaryStage.show();
-        System.out.println("Hello My name");
+
     }
 
 
